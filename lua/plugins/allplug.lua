3af17a51942ca827659039c0f91cd95d04eebd6e
Malenia <kixwalkiki@gmail.com>
2024-02-19 16:48:53 +0100

    first commit

---
+++ lua/plugins/allplug.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
+
+  { "nvim-telescope/telescope.nvim" },
+}
